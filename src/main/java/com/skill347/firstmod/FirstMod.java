c9c15c2eca8bfbcc1cbd18cace4fc8bd67db5160
Maksymilian Tym <tymszczur@gmail.com>
2023-08-10 23:17:25 +0200

    Ore now spawns naturally

--- src/main/java/com/skill347/firstmod/FirstMod.java
+++ src/main/java/com/skill347/firstmod/FirstMod.java
@@ -4,16 +4,27 @@ import com.skill347.firstmod.block.ModBlocks;
 import com.skill347.firstmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.feature.PlacedFeature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class FirstMod implements ModInitializer {
+
 	public static final String MOD_ID = "firstmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static final RegistryKey<PlacedFeature> CUSTOM_ORE_PLACED_KEY = RegistryKey.of(RegistryKeys.PLACED_FEATURE, new Identifier(FirstMod.MOD_ID, "ore_custom"));
+
 
 	@Override
 	public void onInitialize() {
 		ModItems.RegisterModItems();
 		ModBlocks.ResgisterModBlocks();
+		BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, CUSTOM_ORE_PLACED_KEY);
 	}
 }
--- src/main/java/com/skill347/firstmod/block/ModBlocks.java
+++ src/main/java/com/skill347/firstmod/block/ModBlocks.java
@@ -8,21 +8,27 @@ import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 
 public class ModBlocks {
     public static final Block JP2_ORE = registerBlock("jp2ore",
-            new Block(FabricBlockSettings.copyOf(Blocks.IRON_ORE)));
+            new ExperienceDroppingBlock(FabricBlockSettings.copyOf(Blocks.IRON_ORE), UniformIntProvider.create(4, 10)));
+
+    public static final Block JP2_DEEPSLATE_ORE = registerBlock("jp2deepslate_ore",
+            new ExperienceDroppingBlock(FabricBlockSettings.copyOf(Blocks.DEEPSLATE_IRON_ORE), UniformIntProvider.create(4, 10)));
     public static final Block JP2_BLOCK = registerBlock("jp2block",
             new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK)));
 
     private static void addBlocksToIngredientsTabItemGroup(FabricItemGroupEntries entries) {
         entries.add(JP2_ORE);
+        entries.add(JP2_DEEPSLATE_ORE);
         entries.add(JP2_BLOCK);
     }
 
